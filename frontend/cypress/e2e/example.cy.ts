01b60dbea2b98f165b2a65d28d9708719ab27f2f
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2024-01-19 10:39:49 +0100

    initial commit: add base styles and comment out demo-code

---
+++ frontend/cypress/e2e/example.cy.ts
@@ -0,0 +1,9 @@
+// https://on.cypress.io/api
+
+// DEMO:
+// describe('My First Test', () => {
+//   it('visits the app root url', () => {
+//     cy.visit('/')
+//     cy.contains('h1', 'You did it!')
+//   })
+// })
---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+    <header>
+        <img
+            alt="Mascot"
+            class="mascot"
+            src="@/assets/mascot.png"
+            width="125"
+        />
+    </header>
+    <RouterView />
+</template>
