96dfd1b3938b3065a89215754bc573932ed90619
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2024-01-24 16:22:57 +0100

    add: navigation bar and internationalization

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,12 +4,50 @@ import { RouterLink, RouterView } from "vue-router";
 
 <template>
     <header>
-        <img
-            alt="Mascot"
-            class="mascot"
-            src="@/assets/mascot.png"
-            width="125"
-        />
+        <nav id="navigation-bar">
+            <div id="company-name">{{ $t("company.name") }}</div>
+            <div id="routes">
+                <RouterLink to="">{{ $t("navbar.about") }}</RouterLink>
+                <RouterLink to="">{{ $t("navbar.contact") }}</RouterLink>
+                <RouterLink to="">{{ $t("navbar.login") }}</RouterLink>
+                <select v-model="$i18n.locale">
+                    <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</option>
+                </select>
+            </div>
+        </nav>
     </header>
     <RouterView />
 </template>
+
+<style scoped>
+#navigation-bar {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    padding: 1rem 1.5rem;
+    color: white;
+    background-color: var(--gray-800);
+    font-size: 1.25em;
+}
+
+#company-name {
+    font-size: 1.5em;
+}
+
+#routes > a {
+    color: white;
+    text-decoration: none;
+    transition: color 50ms;
+}
+
+#routes > a:hover {
+    color: var(--gray-500);
+}
+
+#routes {
+    flex: 1;
+    display: flex;
+    justify-content: end;
+    gap: 1.5rem;
+}
+</style>
---
+++ frontend/src/internationalization.ts
@@ -0,0 +1,19 @@
+export default {
+    en: {
+        company: {
+            name: "Vet ikke enda"
+        },
+        navbar: {
+            about: "About",
+            contact: "Contact",
+            login: "Log in",
+        }
+    },
+    no: {
+        navbar: {
+            about: "Om",
+            contact: "Kontakt",
+            login: "Logg inn",
+        }
+    }
+};
