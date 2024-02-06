72e59fc6950d47dd27d3079b02748965a40c557d
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2024-02-06 18:24:51 +0100

    fix: add linter config and fix linting errors

--- frontend/src/stores/counter.ts
+++ frontend/src/stores/counter.ts
@@ -1,5 +1,5 @@
-import { ref, computed } from "vue";
-import { defineStore } from "pinia";
+// import { ref, computed } from "vue";
+// import { defineStore } from "pinia";
 
 // DEMO:
 // export const useCounterStore = defineStore("counter", () => {
