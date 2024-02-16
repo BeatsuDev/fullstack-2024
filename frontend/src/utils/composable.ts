48a7b04707a702b4d593a7ba7c73467d764a5e1b
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2024-02-16 16:28:01 +0100

    add: execute on composable

--- frontend/src/utils/composable.ts
+++ frontend/src/utils/composable.ts
@@ -3,18 +3,40 @@ import { ref, type Ref } from "vue";
 export type Composable<T> = {
     data: Ref<T | null>;
     loading: Ref<boolean>;
+    finished: Ref<boolean>;
     error: Ref<unknown>;
+    execute: (...args: any[]) => Promise<T>;
 };
 
-export function useComposable<T>(promise: Promise<T>): Composable<T> {
+export function useComposable<T>(
+    callable: (...args: any[]) => Promise<T>,
+    immediate: boolean = false,
+    ...args: any[]
+): Composable<T> {
     const data = ref<T | null>(null) as Ref<T | null>;
-    const loading = ref<boolean>(true);
+    const loading = ref<boolean>(false);
+    const finished = ref<boolean>(false);
     const error = ref<unknown>(null);
 
-    promise
-        .then((result) => (data.value = result))
-        .catch((err) => (error.value = err))
-        .finally(() => (loading.value = false));
+    const execute = (...overrideArgs: any[]) => {
+        const finalArgs = { ...args, ...overrideArgs };
+        loading.value = true;
 
-    return { data, loading, error };
+        const promise = callable(...finalArgs);
+        promise
+            .then((result) => (data.value = result))
+            .catch((err) => (error.value = err))
+            .finally(() => {
+                loading.value = false;
+                finished.value = true;
+            });
+
+        return promise;
+    };
+
+    if (immediate) {
+        execute();
+    }
+
+    return { data, loading, finished, error, execute };
 }
