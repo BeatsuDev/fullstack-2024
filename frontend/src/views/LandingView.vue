8c0d7bef24613e48d65f0bf2574225b2cb512c7e
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2024-03-19 03:19:10 +0100

    remove: test notifications button

--- frontend/src/views/LandingView.vue
+++ frontend/src/views/LandingView.vue
@@ -1,20 +1,3 @@
-<script setup lang="ts">
-import { useNotificationStore } from "@/stores/notification";
-
-const { addNotification } = useNotificationStore();
-
-function notify() {
-    addNotification({
-        message: "This is a notification!",
-        type: ["info", "warning", "error", "success"][
-            Math.floor(Math.random() * 4)
-        ] as "info" | "warning" | "error" | "success",
-    });
-}
-</script>
-
 <template>
-    <div>
-        <button @click="notify">Notification!</button>
-    </div>
+    <div></div>
 </template>
