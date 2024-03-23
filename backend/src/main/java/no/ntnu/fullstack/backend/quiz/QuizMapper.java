179c27a47f699b7d011aadc77b2365c32f192568
Arunan Gnanasekaran <arunan2212@gmail.com>
2024-03-23 20:03:38 +0100

    feat: add methods for mapping to createdto, quiz and quizdto

--- backend/src/main/java/no/ntnu/fullstack/backend/quiz/QuizMapper.java
+++ backend/src/main/java/no/ntnu/fullstack/backend/quiz/QuizMapper.java
@@ -1,6 +1,24 @@
 package no.ntnu.fullstack.backend.quiz;
 
+import lombok.RequiredArgsConstructor;
+import no.ntnu.fullstack.backend.quiz.dto.QuizDTO;
+import no.ntnu.fullstack.backend.quiz.model.Quiz;
+import no.ntnu.fullstack.backend.quiz.model.Revision;
+import no.ntnu.fullstack.backend.user.UserMapper;
+import no.ntnu.fullstack.backend.user.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper
-public abstract class QuizMapper {}
+@Mapper(uses = UserMapper.class)
+@RequiredArgsConstructor
+public abstract class QuizMapper {
+  @Mapping(source = "revision.title", target = "title")
+  @Mapping(source = "revision.description", target = "description")
+  @Mapping(source = "revision.difficulty", target = "difficulty")
+  @Mapping(source = "quiz.createdAt", target = "createdAt")
+  @Mapping(source = "quiz.creator", target = "creator")
+  public abstract QuizDTO toDTO(Quiz quiz, Revision revision);
+
+  @Mapping(source = "user", target = "creator")
+  public abstract Quiz fromCreateQuiz(User user);
+}
