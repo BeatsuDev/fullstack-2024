2b2ec07d6981f12161de6cc0128e6f16a840e53f
Arunan Gnanasekaran <arunan2212@gmail.com>
2024-03-23 20:05:02 +0100

    feat: add methods for creating quiz in quiz service

--- backend/src/main/java/no/ntnu/fullstack/backend/quiz/QuizService.java
+++ backend/src/main/java/no/ntnu/fullstack/backend/quiz/QuizService.java
@@ -1,10 +1,19 @@
 package no.ntnu.fullstack.backend.quiz;
 
 import lombok.RequiredArgsConstructor;
+import no.ntnu.fullstack.backend.quiz.model.Quiz;
+import no.ntnu.fullstack.backend.quiz.model.Revision;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class QuizService {
   private final QuizRepository quizRepository;
+  private final RevisionRepository revisionRepository;
+
+  public Quiz createQuiz(Quiz quiz, Revision revision) {
+    Quiz createdQuiz = quizRepository.saveAndFlush(quiz);
+    revisionRepository.saveAndFlush(revision);
+    return createdQuiz;
+  }
 }
