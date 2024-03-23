3f56a75e74f917a6e899ab0985aed4c1e42be188
Arunan Gnanasekaran <arunan2212@gmail.com>
2024-03-23 19:59:29 +0100

    feat: add mapper for revision

---
+++ backend/src/main/java/no/ntnu/fullstack/backend/quiz/RevisionMapper.java
@@ -0,0 +1,17 @@
+package no.ntnu.fullstack.backend.quiz;
+
+import no.ntnu.fullstack.backend.quiz.dto.QuizCreateDTO;
+import no.ntnu.fullstack.backend.quiz.model.Revision;
+import no.ntnu.fullstack.backend.user.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper
+public abstract class RevisionMapper {
+
+  @Mapping(source = "user", target = "creator")
+  @Mapping(source = "quizCreate.title", target = "title")
+  @Mapping(source = "quizCreate.description", target = "description")
+  @Mapping(source = "quizCreate.difficulty", target = "difficulty")
+  public abstract Revision fromQuizCreate(QuizCreateDTO quizCreate, User user);
+}
