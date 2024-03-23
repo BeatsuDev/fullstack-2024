9fb1adbc8e0cfc937349e8671d849374d8c5b639
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2024-03-23 21:10:06 +0100

    add: icons

---
+++ frontend/src/assets/icons/navbar/AddIcon.vue
@@ -0,0 +1,21 @@
+<template>
+    <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M1 7C1 4.17157 1 2.75736 1.87868 1.87868C2.75736 1 4.17157 1 7 1H13C15.8284 1 17.2426 1 18.1213 1.87868C19 2.75736 19 4.17157 19 7V13C19 15.8284 19 17.2426 18.1213 18.1213C17.2426 19 15.8284 19 13 19H7C4.17157 19 2.75736 19 1.87868 18.1213C1 17.2426 1 15.8284 1 13V7Z"
+            stroke="currentColor"
+            stroke-width="2"
+        />
+        <path
+            d="M10 6V14M6 10L14 10"
+            stroke="currentColor"
+            stroke-width="3"
+            stroke-linecap="round"
+        />
+    </svg>
+</template>
---
+++ frontend/src/assets/icons/navbar/SearchIcon.vue
@@ -0,0 +1,24 @@
+<template>
+    <svg
+        width="19"
+        height="19"
+        viewBox="0 0 19 19"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <circle
+            cx="7.375"
+            cy="7.375"
+            r="6.375"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+        />
+        <path
+            d="M12.3333 12.3333L18 17.9999"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+        />
+    </svg>
+</template>
