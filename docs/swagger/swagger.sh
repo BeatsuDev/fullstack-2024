adce18857eb9c5bb8fa69aea1cf1151ab1236859
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2024-03-23 14:21:57 +0100

    chore: revert user-related schema changes back to how it is currently implemented

--- docs/swagger/swagger.sh
+++
@@ -1,3 +0,0 @@
-echo 'Starting swagger on http://localhost:3000'
-docker pull swaggerapi/swagger-ui
-docker run -p 3000:8080 -e SWAGGER_JSON=/api.yaml -v $(pwd)/openapi.yaml:/api.yaml swaggerapi/swagger-ui
