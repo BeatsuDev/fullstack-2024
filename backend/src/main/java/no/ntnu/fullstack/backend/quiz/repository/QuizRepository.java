964b5aac3ae0dc20b010d0580dadb160a40abf14
Arunan Gnanasekaran <arunan2212@gmail.com>
2024-03-23 22:51:55 +0100

    feat: add endpoints for retrieving quizzes
    This does not retrieve the latest revision. I do not understand
    jpql, that is for another day...

--- backend/src/main/java/no/ntnu/fullstack/backend/quiz/repository/QuizRepository.java
+++ backend/src/main/java/no/ntnu/fullstack/backend/quiz/repository/QuizRepository.java
@@ -1,7 +1,17 @@
 package no.ntnu.fullstack.backend.quiz.repository;
 
+import java.util.List;
 import java.util.UUID;
 import no.ntnu.fullstack.backend.quiz.model.Quiz;
+import no.ntnu.fullstack.backend.quiz.model.LatestQuiz;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface QuizRepository extends JpaRepository<Quiz, UUID> {}
+public interface QuizRepository extends JpaRepository<Quiz, UUID> {
+
+
+    //TODO: Only return the latest revision for each quiz
+    @Query("SELECT new no.ntnu.fullstack.backend.quiz.model.QuizWithLatestRevision(q, r) FROM Quiz q JOIN Revision r ON q.id = r.quiz.id")
+    List<LatestQuiz> findWithFirstRevision();
+
+}
