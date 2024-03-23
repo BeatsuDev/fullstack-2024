f5cdc7b7ec597d50e5f08b79b63b5d3b7accda4a
Arunan Gnanasekaran <arunan2212@gmail.com>
2024-03-23 18:35:32 +0100

    fix: remove all unused imports and fix compile issues

--- backend/src/main/java/no/ntnu/fullstack/backend/quiz/QuizRepository.java
+++ backend/src/main/java/no/ntnu/fullstack/backend/quiz/QuizRepository.java
@@ -1,10 +1,7 @@
 package no.ntnu.fullstack.backend.quiz;
 
-import java.util.Optional;
 import java.util.UUID;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import no.ntnu.fullstack.backend.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuizRepository extends JpaRepository<User, UUID> {}
--- backend/src/main/java/no/ntnu/fullstack/backend/quiz/QuizService.java
+++ backend/src/main/java/no/ntnu/fullstack/backend/quiz/QuizService.java
@@ -1,12 +1,7 @@
 package no.ntnu.fullstack.backend.quiz;
 
-import java.util.Optional;
-import java.util.UUID;
-
-import org.springframework.stereotype.Service;
-
 import lombok.RequiredArgsConstructor;
-import no.ntnu.fullstack.backend.user.model.User;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
