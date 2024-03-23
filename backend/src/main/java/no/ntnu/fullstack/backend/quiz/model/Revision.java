3a5f8cd50760513c229a9125565fc4b90a4b5972
Arunan Gnanasekaran <arunan2212@gmail.com>
2024-03-23 19:53:27 +0100

    feat: add validation on Revision entity

--- backend/src/main/java/no/ntnu/fullstack/backend/quiz/model/Revision.java
+++ backend/src/main/java/no/ntnu/fullstack/backend/quiz/model/Revision.java
@@ -1,9 +1,12 @@
 package no.ntnu.fullstack.backend.quiz.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import java.util.Date;
 import java.util.UUID;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 import no.ntnu.fullstack.backend.user.model.User;
 import org.hibernate.annotations.CreationTimestamp;
@@ -12,20 +15,19 @@ import org.hibernate.annotations.CreationTimestamp;
 @Setter
 @Entity
 public class Revision {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
 
-    private String title;
+  @NotBlank @NonNull private String title;
 
-    private String description;
+  @NotBlank @NonNull private String description;
 
-    private int difficulty;
+  @Min(1)
+  private int difficulty;
 
-    @ManyToOne(cascade = CascadeType.DETACH)
-    private User creator;
+  @ManyToOne(cascade = CascadeType.DETACH)
+  private User creator;
 
-    @CreationTimestamp
-    private Date createdAt;
-
+  @CreationTimestamp private Date createdAt;
 }
