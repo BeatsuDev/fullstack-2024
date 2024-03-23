87aeb0c62523d384e6352690ede0c22110d4abc1
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2024-03-23 21:33:33 +0100

    fix: remove references to About link in failing cypress tests

--- frontend/cypress/e2e/general.cy.ts
+++ frontend/cypress/e2e/general.cy.ts
@@ -1,19 +1,17 @@
 describe("Locale", () => {
-  it("default locale is norwegian", () => {
-    cy.visit("/");
+    it("default locale is norwegian", () => {
+        cy.visit("/");
 
-    cy.get("#about-router-link").should("contain", "Om");
-    cy.get("#contact-router-link").should("contain", "Kontakt");
-    cy.get("#login-router-link").should("contain", "Logg inn");
-  });
+        cy.get("#contact-router-link").should("contain", "Kontakt");
+        cy.get("#login-router-link").should("contain", "Logg inn");
+    });
 
-  it("change locale to english", () => {
-    cy.visit("/");
+    it("change locale to english", () => {
+        cy.visit("/");
 
-    cy.get("#locale-selector").select("en");
+        cy.get("#locale-selector").select("en");
 
-    cy.get("#about-router-link").should("contain", "About");
-    cy.get("#contact-router-link").should("contain", "Contact");
-    cy.get("#login-router-link").should("contain", "Log in");
-  });
+        cy.get("#contact-router-link").should("contain", "Contact");
+        cy.get("#login-router-link").should("contain", "Log in");
+    });
 });
