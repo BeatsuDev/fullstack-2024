d757143112e36ae7d67d8463a1a335ed4b513383
Arunan Gnanasekaran <arunan2212@gmail.com>
2024-03-23 20:01:10 +0100

    feat: add fields in quiz dto

--- backend/src/main/java/no/ntnu/fullstack/backend/quiz/dto/QuizDTO.java
+++ backend/src/main/java/no/ntnu/fullstack/backend/quiz/dto/QuizDTO.java
@@ -3,8 +3,20 @@ package no.ntnu.fullstack.backend.quiz.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import no.ntnu.fullstack.backend.user.dto.UserDTO;
+import no.ntnu.fullstack.backend.user.model.User;
+
+import java.util.Date;
 
 @Getter
 @Setter
 @NoArgsConstructor
-public class QuizDTO {}
+public class QuizDTO {
+  private String title;
+  private String description;
+  private String difficulty;
+
+  private UserDTO creator;
+
+  private Date createdAt;
+}
