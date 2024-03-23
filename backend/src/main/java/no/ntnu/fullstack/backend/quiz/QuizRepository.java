039394d6e516518c6d01fef6d23f1a69c672c880
Arunan Gnanasekaran <arunan2212@gmail.com>
2024-03-23 20:02:14 +0100

    chore: run format

--- backend/src/main/java/no/ntnu/fullstack/backend/quiz/QuizRepository.java
+++ backend/src/main/java/no/ntnu/fullstack/backend/quiz/QuizRepository.java
@@ -1,7 +1,6 @@
 package no.ntnu.fullstack.backend.quiz;
 
 import java.util.UUID;
-
 import no.ntnu.fullstack.backend.quiz.model.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 
