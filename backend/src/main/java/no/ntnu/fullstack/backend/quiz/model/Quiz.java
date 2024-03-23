21cddeac65d2e08b91b7824b34ae6e77cc782a51
Arunan Gnanasekaran <arunan2212@gmail.com>
2024-03-23 18:07:48 +0100

    feat: add initial quiz and revision models

--- backend/src/main/java/no/ntnu/fullstack/backend/quiz/model/Quiz.java
+++ backend/src/main/java/no/ntnu/fullstack/backend/quiz/model/Quiz.java
@@ -1,12 +1,13 @@
 package no.ntnu.fullstack.backend.quiz.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
+import no.ntnu.fullstack.backend.user.model.User;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Getter
 @Setter
@@ -15,4 +16,12 @@ public class Quiz {
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
+  @ManyToOne(cascade = CascadeType.DETACH)
+  private User creator;
+
+  @CreationTimestamp private Date createdAt;
+
+  @OneToMany(cascade = CascadeType.REMOVE)
+  private List<Revision> revisions;
 }
