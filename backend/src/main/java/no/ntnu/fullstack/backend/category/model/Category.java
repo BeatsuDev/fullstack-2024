f11b3a848e74ca47082d11f1e85993232a89f428
Arunan Gnanasekaran <arunan2212@gmail.com>
2024-03-24 15:07:05 +0100

    feat: add category package

---
+++ backend/src/main/java/no/ntnu/fullstack/backend/category/model/Category.java
@@ -0,0 +1,23 @@
+package no.ntnu.fullstack.backend.category.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String category;
+}
