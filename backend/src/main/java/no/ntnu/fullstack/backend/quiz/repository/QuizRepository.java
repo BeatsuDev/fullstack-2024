5197cd6321d034251fa4163eef2ef5f7b6a7dd5e
Arunan Gnanasekaran <arunan2212@gmail.com>
2024-03-24 14:30:43 +0100

    fix: query that retrieves latest revision

--- backend/src/main/java/no/ntnu/fullstack/backend/quiz/repository/QuizRepository.java
+++ backend/src/main/java/no/ntnu/fullstack/backend/quiz/repository/QuizRepository.java
@@ -9,9 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface QuizRepository extends JpaRepository<Quiz, UUID> {
 
-
-    //TODO: Only return the latest revision for each quiz
-    @Query("SELECT new no.ntnu.fullstack.backend.quiz.model.QuizWithRevision(q, r) FROM Quiz q JOIN Revision r ON q.id = r.quiz.id")
-    List<QuizWithRevision> findWithFirstRevision();
-
+  // TODO: Only return the latest revision for each quiz
+  @Query( " SELECT new no.ntnu.fullstack.backend.quiz.model.QuizWithRevision(q, r) FROM Quiz q JOIN Revision r ON q.id = r.quiz.id WHERE r.createdAt = ( SELECT MAX(r2.createdAt) FROM Revision r2 WHERE r2.quiz.id = q.id )")
+  List<QuizWithRevision> findWithFirstRevision();
 }
