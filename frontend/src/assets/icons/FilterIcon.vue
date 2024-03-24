fe807e0dda453d1bbf1a7cf5a311b5d4e319f08d
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2024-03-24 12:26:32 +0100

    fix: button-component

---
+++ frontend/src/assets/icons/FilterIcon.vue
@@ -0,0 +1,24 @@
+<template>
+    <svg
+        width="20"
+        height="16"
+        viewBox="0 0 20 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M1 4H10M19 4H16"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+        />
+        <path
+            d="M1 12H4M19 12H10"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+        />
+        <circle cx="13" cy="4" r="3" stroke="currentColor" stroke-width="2" />
+        <circle cx="7" cy="12" r="3" stroke="currentColor" stroke-width="2" />
+    </svg>
+</template>
