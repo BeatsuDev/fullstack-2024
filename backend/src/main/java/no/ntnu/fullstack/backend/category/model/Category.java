b724fccdedf1371e5bdf6000981c2c005d14b14a
Arunan Gnanasekaran <arunan2212@gmail.com>
2024-03-24 15:50:06 +0100

    chore add @builder on Category

--- backend/src/main/java/no/ntnu/fullstack/backend/category/model/Category.java
+++ backend/src/main/java/no/ntnu/fullstack/backend/category/model/Category.java
@@ -1,15 +1,13 @@
 package no.ntnu.fullstack.backend.category.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.UUID;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
