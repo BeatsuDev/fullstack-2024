c824ea7fbafdca7621f550b4df17bb64718847b1
Arunan Gnanasekaran <arunan2212@gmail.com>
2024-03-30 17:52:59 +0100

    fix: add QuizHero component

---
+++ frontend/src/components/QuizHero.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="header">
+        <h1 style="margin-top: 0">{{ props.quiz.title }}</h1>
+        <div class="action-bar">
+            <ButtonComponent filled large>Play</ButtonComponent>
+        </div>
+        <h3>Description</h3>
+        <p>{{ props.quiz.description }}</p>
+        <ButtonComponent v-if="props.editable" @click="emit('edit', props.quiz)">Edit</ButtonComponent>
+    </div>
+</template>
+<script lang="ts" setup>
+import ButtonComponent from "./ButtonComponent.vue";
+
+import type { Quiz } from "@/api";
+
+const props = defineProps<{
+    quiz: Quiz;
+    editable?: boolean;
+}>();
+
+const emit = defineEmits<{
+    (event: "edit", quiz: Quiz): void;
+}>();
+</script>
+<style scoped>
+
+.header {
+    padding: 1rem;
+    border-bottom: 1px solid var(--primary-200);
+    border-radius: 1rem;
+    background-color: var(--primary-200);
+}
+
+.action-bar {
+    justify-content: end;
+    width: 100%;
+    display: flex;
+}
+</style>
