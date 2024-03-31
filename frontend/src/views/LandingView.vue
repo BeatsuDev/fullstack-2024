bf9617ae789c366e991807300bc32a36b9719ec5
Arunan Gnanasekaran <arunan2212@gmail.com>
2024-03-31 17:03:23 +0200

    feat: add hero on landing page

--- frontend/src/views/LandingView.vue
+++ frontend/src/views/LandingView.vue
@@ -1,3 +1,48 @@
 <template>
-    <main></main>
+    <main>
+        <div class="hero">
+            <div class="hero-text">
+                <div class="hero-title">Kazoot!</div>
+                <div>
+                    <p>{{ randomSubTitle() }} </p>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
+<style scoped>
+    .hero {
+        background-image: url("src/assets/stacked-peaks-haikei.svg");
+        background-size: cover;
+        background-position: center;
+        height: calc(100svh - 68px);
+        display: flex;
+        color: white;
+    }
+
+    .hero-text {
+        margin-top: 10rem;
+        margin-left: 2rem;
+        font-size: 1.5rem;
+    }
+
+    .hero-title {
+        font-size: 6rem;
+
+    }
+</style>
+<script setup>
+
+const subtitles = [
+    "Did you mean to go to Kahoot?",
+    "Kazoot! The best quiz app",
+    "Kahoot who? Kazoot!",
+    "Let's get quizzical",
+    "Learning is fun with Kazoot!",
+]
+
+function randomSubTitle() {
+    return subtitles[Math.floor(Math.random() * subtitles.length)];
+
+}
+</script>
