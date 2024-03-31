591f7464751f018302a7e582da07dcb544a041bd
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2024-03-31 19:29:47 +0200

    refac

--- frontend/src/components/QuizGrid.vue
+++ frontend/src/components/QuizGrid.vue
@@ -1,48 +1,15 @@
 <script setup lang="ts">
-import type { QuizOverview, Category } from "@/api";
-import router from "@/router";
+import type { QuizOverview } from "@/api";
+import QuizOverviewCard from "@/components/QuizOverviewCard.vue";
 
 defineProps<{
     quizzes: QuizOverview[];
 }>();
-
-function onQuizCardClick(quiz: QuizOverview) {
-    router.push({ name: "quiz", params: { id: quiz.id } });
-}
-
-function getCategoryStyle(category: Category) {
-    return {
-        border: `2px solid ${category.color}`,
-    };
-}
 </script>
 
 <template>
     <div class="found-quizzes-grid">
-        <div
-            v-for="(quiz, i) in quizzes"
-            :key="i"
-            class="quiz-overview-card"
-            @click="onQuizCardClick(quiz)"
-        >
-            <div
-                class="quiz-card-banner"
-                :style="`background-color: hsl(${Math.random() * 360}deg ${60 + Math.random() * 40}% 50%);`"
-            ></div>
-            <div class="quiz-card-content">
-                <div class="quiz-card-categories">
-                    <div
-                        v-for="(category, i) in quiz.categories"
-                        :key="i"
-                        :style="getCategoryStyle(category)"
-                    >
-                        {{ category.name }}
-                    </div>
-                </div>
-                <h4 style="margin-top: 0">{{ quiz.title }}</h4>
-                <p>{{ quiz.description }}</p>
-            </div>
-        </div>
+        <QuizOverviewCard v-for="(quiz, i) in quizzes" :key="i" :quiz="quiz" />
     </div>
 </template>
 
@@ -51,41 +18,5 @@ function getCategoryStyle(category: Category) {
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
     gap: 1em;
-}
-
-.quiz-overview-card {
-    border: 1px solid black;
-    border-radius: 1em;
-    transition-duration: 150ms;
-}
-
-.quiz-overview-card:hover {
-    box-shadow: 2px 2px 4px rgb(0 0 0 / 50%);
-    transform: translate(-2px, -2px);
-    cursor: pointer;
-}
-
-.quiz-overview-card .quiz-card-banner {
-    border-top-left-radius: 1em;
-    border-top-right-radius: 1em;
-    height: 5em;
-}
-
-.quiz-overview-card .quiz-card-content {
-    padding: 1em;
-}
-
-.quiz-overview-card .quiz-card-categories {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 0.5em;
-    margin-bottom: 0.5em;
-}
-
-.quiz-overview-card .quiz-card-categories div {
-    padding: 0.25em 0.5em;
-    border-radius: 1em;
-    font-size: 0.8em;
-    text-transform: uppercase;
 }
 </style>
