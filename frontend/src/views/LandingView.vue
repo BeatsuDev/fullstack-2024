9eb9597ba41065b6d8e107612c73178ffbbda139
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2024-03-31 17:09:40 +0200

    fix: height of landing page

--- frontend/src/views/LandingView.vue
+++ frontend/src/views/LandingView.vue
@@ -4,15 +4,19 @@
             <div class="hero-text">
                 <div class="hero-title">Kazoot!</div>
                 <div>
-                    <p>{{ randomSubTitle() }} </p>
-                    <button-component @click="router.push('/login')" filled rounded-lg>Log in</button-component>
+                    <p>{{ randomSubTitle() }}</p>
+                    <button-component
+                        @click="router.push('/login')"
+                        filled
+                        rounded-lg
+                        >Log in</button-component
+                    >
                 </div>
             </div>
         </div>
     </main>
 </template>
 <script setup>
-
 import ButtonComponent from "@/components/ButtonComponent.vue";
 import { useRouter } from "vue-router";
 
@@ -24,31 +28,29 @@ const subtitles = [
     "Kahoot who? Kazoot!",
     "Let's get quizzical",
     "Learning is fun with Kazoot!",
-]
+];
 
 function randomSubTitle() {
     return subtitles[Math.floor(Math.random() * subtitles.length)];
-
 }
 </script>
 <style scoped>
-    .hero {
-        background-image: url("src/assets/stacked-peaks-haikei.svg");
-        background-size: cover;
-        background-position: center;
-        height: calc(100svh - 68px);
-        display: flex;
-        color: white;
-    }
+.hero {
+    background-image: url("src/assets/stacked-peaks-haikei.svg");
+    background-size: cover;
+    background-position: center;
+    height: calc(100svh - 66px);
+    display: flex;
+    color: white;
+}
 
-    .hero-text {
-        margin-top: 10rem;
-        margin-left: 2rem;
-        font-size: 1.5rem;
-    }
+.hero-text {
+    margin-top: 10rem;
+    margin-left: 2rem;
+    font-size: 1.5rem;
+}
 
-    .hero-title {
-        font-size: 6rem;
-
-    }
+.hero-title {
+    font-size: 6rem;
+}
 </style>
