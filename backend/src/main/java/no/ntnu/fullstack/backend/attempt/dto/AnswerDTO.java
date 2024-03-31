1e33c080c932e62e69100c8d2fdb340c1afbb09d
Sebastian Bugge <vkbugge@hotmail.com>
2024-03-31 16:02:32 +0200

    fix: add setters to AnswerDTO

--- backend/src/main/java/no/ntnu/fullstack/backend/attempt/dto/AnswerDTO.java
+++ backend/src/main/java/no/ntnu/fullstack/backend/attempt/dto/AnswerDTO.java
@@ -2,8 +2,10 @@ package no.ntnu.fullstack.backend.attempt.dto;
 
 import java.util.UUID;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class AnswerDTO {
   private UUID questionId;
   private String answer;
