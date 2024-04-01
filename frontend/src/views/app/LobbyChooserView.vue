55e968530cf62680854d54f8bd50f29c6b0fcf61
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2024-04-01 20:46:16 +0200

    add: join lobby by code

--- frontend/src/views/app/LobbyChooserView.vue
+++ frontend/src/views/app/LobbyChooserView.vue
@@ -1,24 +1,73 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import { type AxiosError, isAxiosError } from "axios";
+import { useNotificationStore } from "@/stores/notification";
 import ButtonComponent from "@/components/ButtonComponent.vue";
 
+import router from "@/router";
+import { CompetitionApi } from "@/api";
+import { useMultiplayerStore } from "@/stores/multiplayer";
+
 const lobbyCode = ref("");
+
+async function joinLobby() {
+    const multiplayerApi = new CompetitionApi();
+
+    let response;
+    try {
+        response = await multiplayerApi.joinCompetition(
+            parseInt(lobbyCode.value)
+        );
+    } catch (e: any) {
+        if (!(e instanceof Error) && !isAxiosError(e)) {
+            return useNotificationStore().addNotification({
+                message:
+                    "Could not join lobby. Unknown error. Please try again later.",
+                type: "error",
+            });
+        }
+
+        const error = e as Error | AxiosError;
+
+        if (error instanceof Error && !isAxiosError(error)) {
+            return useNotificationStore().addNotification({
+                message:
+                    "Could not join lobby. Please try again later. " + error,
+                type: "error",
+            });
+        }
+
+        const axiosError = error as AxiosError;
+
+        if (axiosError.status) {
+            switch (axiosError.status) {
+                case 404:
+                    return useNotificationStore().addNotification({
+                        message: "Lobby not found. Please try again.",
+                        type: "error",
+                    });
+            }
+        }
+
+        return useNotificationStore().addNotification({
+            message:
+                "Could not join lobby. Unknown error. Please try again later.",
+            type: "error",
+        });
+    }
+
+    useMultiplayerStore().players = response.data.competition.competitors;
+    router.push({
+        name: "quiz-lobby",
+        params: { lobbyCode: lobbyCode.value },
+    });
+}
 </script>
 
 <template>
     <div class="lobby-chooser-container">
         <input v-model="lobbyCode" type="text" placeholder="123456" />
-        <ButtonComponent
-            large
-            @click="
-                $router.push({
-                    name: 'quiz-lobby',
-                    params: { lobbyCode },
-                })
-            "
-        >
-            Join Lobby
-        </ButtonComponent>
+        <ButtonComponent large @click="joinLobby"> Join Lobby </ButtonComponent>
     </div>
 </template>
 
