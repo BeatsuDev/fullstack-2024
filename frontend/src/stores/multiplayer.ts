ae2f141d356d11a7a7bf33f86894f0d5f662f6fb
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2024-04-01 23:24:12 +0200

    add check if user is in a multiplayer game currently

--- frontend/src/stores/multiplayer.ts
+++ frontend/src/stores/multiplayer.ts
@@ -5,8 +5,10 @@ import type { Competitor } from "@/api";
 export const useMultiplayerStore = defineStore("multiplayer", () => {
     const players = ref<Competitor[]>([]);
     const lobbyCode = ref<number | null>(null);
+    const isInMultiplayerGame = ref(false);
 
     return {
+        isInMultiplayerGame,
         lobbyCode,
         players,
     };
