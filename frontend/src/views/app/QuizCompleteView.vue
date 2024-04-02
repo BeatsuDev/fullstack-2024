0f62deb856c90f26a317515dca826d48a2cbbaba
Eric Bieszczad-Stie <ericbieszczad@gmail.com>
2024-04-02 03:38:32 +0200

    fix: reset mulitplayer store if someone leaaves midway

--- frontend/src/views/app/QuizCompleteView.vue
+++ frontend/src/views/app/QuizCompleteView.vue
@@ -1,3 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import router from "@/router";
+
+const attemptId = router.currentRoute.value.query.attemptId as string;
+</script>
 
 <template>Wow! You completed the quiz! Good job!</template>
