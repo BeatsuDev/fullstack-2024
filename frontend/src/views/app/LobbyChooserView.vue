ec891644fae338fe2ac99f3e2f122aba5f53278b
Sebastian Bugge <vkbugge@hotmail.com>
2024-04-03 01:24:09 +0200

    fix: remove uneeded code

--- frontend/src/views/app/LobbyChooserView.vue
+++ frontend/src/views/app/LobbyChooserView.vue
@@ -1,71 +1,12 @@
 <script lang="ts" setup>
 import { ref } from "vue";
-import { type AxiosError, isAxiosError } from "axios";
-import { useNotificationStore } from "@/stores/notification";
 import ButtonComponent from "@/components/ButtonComponent.vue";
 
 import router from "@/router";
-import { CompetitionApi } from "@/api";
-import { useMultiplayerStore } from "@/stores/multiplayer";
 
 const lobbyCode = ref("");
 
 async function joinLobby() {
-    const multiplayerApi = new CompetitionApi();
-
-    let response;
-    try {
-        response = await multiplayerApi.joinCompetition(
-            parseInt(lobbyCode.value)
-        );
-    } catch (e: any) {
-
-        if (e.response?.status == 404) {
-            useNotificationStore().addNotification({
-                message: "The lobby was not found. ",
-                type: "error",
-            });
-            return;
-
-        }
-        if (!(e instanceof Error) && !isAxiosError(e)) {
-            return useNotificationStore().addNotification({
-                message:
-                    "Could not join lobby. Unknown error. Please try again later.",
-                type: "error",
-            });
-        }
-
-        const error = e as Error | AxiosError;
-
-        if (error instanceof Error && !isAxiosError(error)) {
-            return useNotificationStore().addNotification({
-                message:
-                    "Could not join lobby. Please try again later. " + error,
-                type: "error",
-            });
-        }
-
-        const axiosError = error as AxiosError;
-
-        if (axiosError.status) {
-            switch (axiosError.status) {
-                case 404:
-                    return useNotificationStore().addNotification({
-                        message: "Lobby not found. Please try again.",
-                        type: "error",
-                    });
-            }
-        }
-
-        return useNotificationStore().addNotification({
-            message:
-                "Could not join lobby. Unknown error. Please try again later.",
-            type: "error",
-        });
-    }
-
-    useMultiplayerStore().multiplayerData = response.data;
     router.push({
         name: "quiz-lobby",
         params: { lobbyCode: lobbyCode.value },
